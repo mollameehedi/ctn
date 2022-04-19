2ee9fd68c91b247d5ce657f8542f81f38bc277db
mollameehedi <mollameehedi@gmail.com>
2022-04-20 01:52:35 +0600

    Design almost complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,4 +13,27 @@ $(function () {
 
 });
 
+function mainmenu() {
+  jQuery(".navdropdon").hover(function () {
+//alert("hai-2");
+      jQuery(this).find('.dropdon').slideDown("fast");
+  }, function () {
+      jQuery(this).find('.dropdon').slideUp("fast");
+  });
+}
+function megamenu() {
+  jQuery(".navMega").hover(function () {
+//alert("hai-2");
+      jQuery(this).find('.megamenu').slideDown("fast");
+  }, function () {
+      jQuery(this).find('.megamenu').slideUp("fast");
+  });
+}
+
+
+$(document).ready(function () {
+  mainmenu();
+  megamenu();
+}); 
+
 
